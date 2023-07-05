cdfbeca09e825843d1e722c9b30743a6d7156559
MertY <mertyigit148@gmail.com>
2023-07-05 15:24:10 +0300

    Authentication methods has applied via Firebase.

--- app/src/main/java/com/example/quickrecipes/LoginFragment.java
+++ app/src/main/java/com/example/quickrecipes/LoginFragment.java
@@ -1,18 +1,33 @@
 package com.example.quickrecipes;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.quickrecipes.databinding.FragmentLoginBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginFragment extends Fragment {
 
-
-
+    private FragmentLoginBinding binding;
+    private FirebaseAuth mAuth;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -24,13 +39,99 @@ public class LoginFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user!= null){
+            Intent intent = new Intent(requireContext(), RecipesActivity.class);
+            startActivity(intent);
+            finishActivity();
+        }
 
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        // Fragmentin görsel arayüzünü oluşturmak için inflater
+
+        // ViewBinding oluşturma
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+
+        // ViewBinding ile bağlı öğeleri kullan
+        View view = binding.getRoot();
+
+        // ViewBinding'in kök görünümünü döndür
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        Button loginButton = view.findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginClicked(view);
+
+            }
+        });
+
+        TextView registernow = view.findViewById(R.id.registerText);
+        registernow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerNowClicked(view);
+            }
+        });
+    }
+
+        public void loginClicked(View view){
+
+        String email = binding.editTextTextEmailAddress.getText().toString();
+        String password = binding.editTextPassword.getText().toString();
+
+        if(email.equals("")||password.equals("")){
+
+            Toast.makeText(requireContext(), "Please fill in all the necessary fields", Toast.LENGTH_LONG).show();
+
+        }else{
+            mAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(requireContext(),RecipesActivity.class);
+                    startActivity(intent);
+                    finishActivity();
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(requireContext(),e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+
+    }
+
+    public void registerNowClicked(View view){
+
+        NavDirections action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment();
+        Navigation.findNavController(view).navigate(action);
+
+    }
+    private void finishActivity() {
+        if(getActivity() != null) {
+            getActivity().finish();
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/quickrecipes/RegisterFragment.java
+++ app/src/main/java/com/example/quickrecipes/RegisterFragment.java
@@ -1,17 +1,29 @@
 package com.example.quickrecipes;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.quickrecipes.databinding.FragmentRegisterBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class RegisterFragment extends Fragment {
 
-
+    private FragmentRegisterBinding binding;
+    private FirebaseAuth mAuth;
     public RegisterFragment() {
         // Required empty public constructor
     }
@@ -20,13 +32,84 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        // Fragmentin görsel arayüzünü oluşturmak için inflater'ı kullanın
+
+        // ViewBinding oluşturma
+        binding = FragmentRegisterBinding.inflate(inflater, container, false);
+
+        // ViewBinding ile bağlı öğeleri kullanabilirsiniz
+        View view = binding.getRoot();
+
+        // ViewBinding'in kök görünümünü döndürün
+        return view;
+    }
+
+
+
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        Button registerButton = view.findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerClicked(view);
+
+            }
+        });
+    }
+    public void registerClicked(View view){
+        String email = binding.editTextTextEmailAddress2.getText().toString();
+        String password = binding.editTextTextPassword.getText().toString();
+        String passwordConfirm = binding.editTextTextPassword2.getText().toString();
+
+        if(email.equals("")||password.equals("")||passwordConfirm.equals("")){
+
+            Toast.makeText(requireContext(), "Please fill in all the necessary fields", Toast.LENGTH_LONG).show();
+
+
+
+        }else if(password.equals(passwordConfirm)) {
+            mAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(requireContext(), RecipesActivity.class);
+                    startActivity(intent);
+                    finishActivity();
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(requireContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+
+                }
+            });
+
+        }
+
+    }
+
+
+
+
+
+    private void finishActivity() {
+        if(getActivity() != null) {
+            getActivity().finish();
+        }
     }
 }
