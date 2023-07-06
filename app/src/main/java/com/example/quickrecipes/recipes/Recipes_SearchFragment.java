1395c8f890cd9a25ecaf7f47f289ac6be10957f9
MertY <mertyigit148@gmail.com>
2023-07-06 14:29:48 +0300

    RecyclerView added.

--- app/src/main/java/com/example/quickrecipes/recipes/Recipes_SearchFragment.java
+++ app/src/main/java/com/example/quickrecipes/recipes/Recipes_SearchFragment.java
@@ -2,7 +2,11 @@ package com.example.quickrecipes.recipes;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +15,13 @@ import android.view.ViewGroup;
 import com.example.quickrecipes.R;
 
 public class Recipes_SearchFragment extends Fragment {
+
+    private RecyclerView recylerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    RecyclerViewAdapter recyclerViewAdapter;
+
+    int []arr = {R.drawable.chocolate_sauce,R.drawable.omelet,R.drawable.fried_chicken,R.drawable.berrypie};
 
 
     public Recipes_SearchFragment() {
@@ -23,6 +34,8 @@ public class Recipes_SearchFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
     }
 
     @Override
@@ -30,5 +43,22 @@ public class Recipes_SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recipes__search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recylerView = view.findViewById(R.id.recyclerView);
+        layoutManager = new GridLayoutManager(getContext(),2);
+        recylerView.setLayoutManager(layoutManager);
+
+        recyclerViewAdapter = new RecyclerViewAdapter(arr);
+        recylerView.setAdapter(recyclerViewAdapter);
+        recylerView.setHasFixedSize(true);
+
+
+
+
     }
 }
